e52922e589b9e59f3bd24b4cd87604f8ac591d42
Kai-1201 <karim.assimov@narxoz.kz>
2025-04-24 22:48:44 +0500

    Adding ReverseSeasonIterator

---
+++ src/Streaming_Service_Binge_Mode/ReverseSeasonIterator.java
@@ -0,0 +1,23 @@
+package Streaming_Service_Binge_Mode;
+
+import java.util.List;
+
+public class ReverseSeasonIterator implements EpisodeIterator {
+    private List<Episode> episodes;
+    private int position;
+
+    public ReverseSeasonIterator(List<Episode> episodes) {
+        this.episodes = episodes;
+        this.position = episodes.size() - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position >= 0;
+    }
+
+    @Override
+    public Episode next() {
+        return episodes.get(position--);
+    }
+}
